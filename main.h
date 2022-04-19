fb9be9b74f914b206763c1e904e5fab7cbfc647a
GikonyoJohn <gikonyojohn39@gmail.com>
2022-04-19 16:45:35 +0300

    main

--- main.h
+++ main.h
@@ -10,3 +10,22 @@ int _strlen(char *i);
 int _printf(const char *format, ...);
 void _puts(char *strn);
 char *convert_to(char get[], unsigned int num, int base);
+int print_char(va_list arguments);
+int print_str(va_list arguments);
+int print_nbr(va_list arguments);
+int print_binary(va_list arguments);
+int print_octal(va_list arguments);
+int print_hexa_lower(va_list arguments);
+int print_hexa_upper(va_list arguments);
+int print_unsigned(va_list arguments);
+int print_str_unprintable(va_list arguments);
+int print_str_reverse(va_list arguments);
+int print_ptr(va_list arguments);
+int print_rot13(va_list arguments);
+int print_percent(va_list arguments __attribute__((unused)));
+
+typedef struct flags
+{
+unsigned char flag;
+unsigned char value;
+} flag_t
